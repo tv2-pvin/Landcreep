ddbdf40988476841a95dbe11fb85cc78a6881b96
tv2-pvin <pvin@tv2.dk>
2020-05-10 13:52:09 +0200

    complete change of direction

--- control.lua
+++ control.lua
@@ -1,3 +1,11 @@
+script.on_init(function()
+	global.landcreepers_has_run = false
+end)
+
+script.on_configuration_changed(function()
+	global.landcreepers_has_run = false
+end)
+
 local function addToSet(set, key)
     set[key] = true
 end
@@ -6,30 +14,8 @@ local function setContains(set, key)
     return set[key] ~= nil
 end
 
-local function addRoboport(roboport, opRadius, checked, timeout, timesChecked)
-	table.insert(
-		global.landcreepers,
-		{
-			port=roboport,
-			x=roboport.position.x,
-			y=roboport.position.y,
-			op_radius=opRadius,
-			radius=roboport.logistic_cell.construction_radius * settings.global["landcreep_range"].value / 100,
-			checked=checked,
-			timeout=timeout,
-			timesChecked=timesChecked
-		}
-	)
-end
-
-local function updateRoboport(index, roboport, opRadius, checked, timeout, timesChecked)
-	table.remove(global.landcreepers, index)
-	addRoboport(roboport, opRadius, checked, timeout, timesChecked)
-end
-
-
-
 local function initWaterTiles()
+	global.landcreepers_water_tiles = {}
 	--- vanilla
 	if not setContains(global.landcreepers_water_tiles, "water") then
 		addToSet(global.landcreepers_water_tiles, "water")
@@ -57,133 +43,36 @@ local function initWaterTiles()
 	end
 end
 
-local function init()
-	global.landcreepers_loaded=false
-	global.landcreepers = {}
-	global.landcreepers_water_tiles = {}
-	global.landcreepers_tile = settings.global['landcreep_landfill_tile_override'].value
-	if global.landcreepers_tile ~= nil then
-		global.landcreepers_tile = "landfill"
-	end
-	initWaterTiles()
-	for _, surface in pairs(game.surfaces) do
-		for _, roboport in pairs(surface.find_entities_filtered{type="roboport"}) do
-			addRoboport(roboport, 1, false, 0, 0)
-		end
-	end
-	global.landcreepers_loaded=true
-end
 
-
-
-local function isWaterTile(tile)
-	return setContains(global.landcreepers_water_tiles, tile.name)
-end
-
-local function placeLandfill(roboport, numberOfBotsSent, amount, tile)
-	if isWaterTile(tile) and roboport.logistic_network.get_item_count(global.landcreepers_tile) > 0 and numberOfBotsSent < amount then
-		if roboport.surface.can_place_entity{name="tile-ghost", position={tile.position.x, tile.position.y}, inner_name=global.landcreepers_tile, force=roboport.force} then
-			roboport.surface.create_entity{name="tile-ghost", position={tile.position.x, tile.position.y}, inner_name=global.landcreepers_tile, force=roboport.force, expires=false}
-			numberOfBotsSent = numberOfBotsSent + 1
-		end
-	end
-	return numberOfBotsSent
-end
-
-local function iter(index, roboport, amount, opRadius, radius, timeout, timesChecked)
-	local numberOfBotsSent = 0
-	for xx = -opRadius, opRadius-1, 1 do
-		for yy = -opRadius, opRadius-1, 1 do
-			if xx <= -opRadius+1 or xx >= opRadius-2 or yy <= -opRadius+1 or yy >= opRadius-2 then --Check only the outer ring, width 2.
+local function landcreep(roboport)
+	if roboport and roboport.valid and roboport.logistic_cell and roboport.logistic_cell.valid and roboport.logistic_cell.construction_radius > 0 then
+		local radius = roboport.logistic_cell.construction_radius
+		for xx = -radius, radius, 1 do
+			for yy = -radius, radius, 1 do
 				local tile = roboport.surface.get_tile(roboport.position.x + xx, roboport.position.y + yy)
-				if numberOfBotsSent < amount and isWaterTile(tile) then
-					numberOfBotsSent = placeLandfill(roboport, numberOfBotsSent, amount, tile)
+				if roboport.surface.can_place_entity{name="tile-ghost", position={tile.position.x, tile.position.y}, inner_name=global.landcreepers_tile, force=roboport.force} then
+					roboport.surface.create_entity{name="tile-ghost", position={tile.position.x, tile.position.y}, inner_name=global.landcreepers_tile, force=roboport.force, expires=false}
 				end
 			end
 		end
 	end
-	updateRoboport(index, roboport, opRadius+1, radius == opRadius+1, timeout, timesChecked)
 end
 
-local function getAmountOfRobotsToSend(roboport)
-	local constructionFactor = settings.global["landcreep_construction_factor"].value
-	local maxSetting = settings.global['landcreep_maximum_number_of_orders'].value
-	if maxSetting == 0 then
-		return math.min(math.max(math.floor(roboport.logistic_network.available_construction_robots / constructionFactor), 1), roboport.logistic_network.get_item_count(global.landcreepers_tile))
-	else
-		return math.min(maxSetting, math.min(math.max(math.floor(roboport.logistic_network.available_construction_robots / constructionFactor), 1), roboport.logistic_network.get_item_count(global.landcreepers_tile)))
-	end
-end
-
-local function landfill(index)
-	local roboport = global.landcreepers[index]
-	local port = roboport.port
-	if roboport and not roboport.checked then
-		if port.valid and port.logistic_network and port.logistic_network.valid and port.logistic_cell and port.logistic_cell.valid then
-			if port.logistic_network.get_item_count(global.landcreepers_tile) > 0 then
-				local amount = getAmountOfRobotsToSend(port)
-				iter(index, port, amount, roboport.op_radius, roboport.radius, roboport.timeout, roboport.timesChecked)
+script.on_nth_tick(60, function()
+	if not global.landcreepers_has_run then
+		initWaterTiles()
+		for _, surface in pairs(game.surfaces) do
+			for _, roboport in pairs(surface.find_entities_filtered{type="roboport"}) do
+				landcreep(roboport)
 			end
-		else
-			table.remove(global.landcreepers, index)
 		end
-	else
-		if roboport.timeout >= 1000 then
-			if roboport.timesChecked >= 5 then
-				table.remove(global.landcreepers, index)
-			else
-				updateRoboport(index, roboport.port, 1, false, 0, roboport.timesChecked+1)
-			end
-		else
-			updateRoboport(index, roboport.port, roboport.op_radius, roboport.checked, roboport.timeout+1, roboport.timesChecked)
-		end
+		global.landcreepers_has_run = true
 	end
-end
-
-script.on_init(function()
-	local status, err = pcall(function()
-		init()
-		global.landcreepers_loaded = true
-	end)
-	if not status then
-		log(err)
-		global.landcreepers_loaded = false
-	end
-end)
-
-script.on_configuration_changed(function()
-	local status, err = pcall(function()
-		init()
-		global.landcreepers_loaded = true
-	end)
-	if not status then
-		log(err)
-		global.landcreepers_loaded = false
-	end
-end)
-
-script.on_nth_tick(settings.global["landcreep_tick_interval"].value, function()
-	local status, err = pcall(function()
-		if not global.landcreepers_loaded then
-			init()
-		end
-		for i=1, #global.landcreepers, 1 do
-			landfill(i)
-		end
-	end)
-	if not status then
-		log(err)
-		global.landcreepers_loaded = false
-	end
-end)
-
-script.on_nth_tick(10000*settings.global['landcreep_tick_interval'].value, function()
-	global.landcreepers_loaded = false
 end)
 
 local function handleEntityBuiltEvent(event)
 	if event.created_entity and event.created_entity.valid and event.created_entity.type == "roboport" then
-		addRoboport(event.created_entity, 1, false)
+		landcreep(event.created_entity)
 	end
 end
 
