e9ded07703c895218eaa60801f0ad3cf0706f3fc
tv2-pvin <pvin@tv2.dk>
2020-06-24 22:18:10 +0200

    1.2.5

--- control.lua
+++ control.lua
@@ -1,150 +1,72 @@
+local function placeTile(surface, tile, roboport, tile_name)
+	if surface.can_place_entity{name="tile-ghost", position=tile.position, inner_name=tile_name, force=roboport.force, build_check_type=defines.build_check_type.ghost_place, forced=true} then
+		surface.create_entity{name="tile-ghost", position=tile.position, inner_name=tile_name, force=roboport.force}
+	end
+end
+
+local function landcreep(item)
+	local roboport = item.roboport
+	local surface = item.surface
+	local tile_name = settings.global["landcreep_tile_override"].value or "landfill"
+
+	if roboport and roboport.valid and roboport.logistic_cell and roboport.logistic_cell.valid and roboport.logistic_cell.construction_radius > 0 then
+
+		local radius = roboport.logistic_cell.construction_radius
+		local tileCandidates
+
+		if settings.global["landcreepers_circular_creep"].value == true then
+			tileCandidates = surface.find_tiles_filtered{position=roboport.position, radius=radius, collision_mask="water-tile"}
+		else
+			local xx = roboport.position.x
+			local yy = roboport.position.y
+			local area = {{xx-radius, yy-radius}, {xx+radius, yy+radius}}
+			tileCandidates = surface.find_tiles_filtered{area=area, collision_mask="water-tile"}
+		end
+
+		for _, tile in pairs(tileCandidates) do
+			placeTile(surface, tile, roboport, tile_name)
+		end
+
+		global.landcreepers.index = global.landcreepers.index + 1
+	else
+		table.remove(global.landcreepers.roboports, global.landcreepers.roboport_index)
+	end
+
+end
+
 script.on_init(function()
-	global.landcreepers_has_run = false
+	global.landcreepers = {}
 end)
 
-local function readSetting(key, defaultValue)
-	if settings.global[key] ~= nil then
-		return settings.global[key].value
-	end
-	return defaultValue
-end
-
-local function addToSet(set, key)
-    set[key] = true
-end
-
-local function setContains(set, key)
-    return set[key] ~= nil
-end
-
-
-local function initWaterTiles()
-	global.landcreepers_water_tiles = {}
-	--- vanilla
-	if not setContains(global.landcreepers_water_tiles, "water") then
-		addToSet(global.landcreepers_water_tiles, "water")
-	end
-	if not setContains(global.landcreepers_water_tiles, "water-green") then
-		addToSet(global.landcreepers_water_tiles, "water-green")
-	end
-	if not setContains(global.landcreepers_water_tiles, "water-mud") then
-		addToSet(global.landcreepers_water_tiles, "water-mud")
-	end
-	if not setContains(global.landcreepers_water_tiles, "water-shallow") then
-		addToSet(global.landcreepers_water_tiles, "water-shallow")
-	end
-	if not setContains(global.landcreepers_water_tiles, "deepwater") then
-		addToSet(global.landcreepers_water_tiles, "deepwater")
-	end
-	if not setContains(global.landcreepers_water_tiles, "deepwater-green") then
-		addToSet(global.landcreepers_water_tiles, "deepwater-green")
-	end
-	--- spaceblock
-	for i=1, 8, 1 do
-		if not setContains(global.landcreepers_water_tiles, "space-tile-"..i) then
-			addToSet(global.landcreepers_water_tiles, "space-tile-"..i)
-		end
-	end
-end
-
-local function placeLandfill(roboport, tile)
-	local tile_name = readSetting("landcreep_tile_override", "landfill")
-	if roboport.surface.can_place_entity{name="tile-ghost", position={tile.position.x, tile.position.y}, inner_name=tile_name, force=roboport.force} then
-		roboport.surface.create_entity{name="tile-ghost", position={tile.position.x, tile.position.y}, inner_name=tile_name, force=roboport.force, expires=false}
-		return 1
-	end
-	return 0
-end
-
-local function hasValidLogisticsNetwork(roboport)
-	return roboport.logistic_network and roboport.logistic_network.valid
-end
-
-local function hasItem(roboport)
-	return hasValidLogisticsNetwork(roboport) and roboport.logistic_network.get_item_count(readSetting("landcreep_tile_override", "landfill")) > 0
-end
-
-local function hasRobots(roboport)
-	return hasValidLogisticsNetwork(roboport) and roboport.logistic_network.available_construction_robots > 0
-end
-
-local function landcreep(roboport)
-	if roboport and roboport.valid and roboport.logistic_cell and roboport.logistic_cell.valid and roboport.logistic_cell.construction_radius > 0 then
-		local radius = roboport.logistic_cell.construction_radius
-		local xStart = -radius
-		local xStop = radius-1
-		local x = 0
-		if readSetting("landcreepers_iterate_less_than_one_roboport", true) then
-			xStart = global.landcreepers_roboport_index_x or xStart
-			xStop = math.min(xStart + readSetting("landcreepers_iterate_less_than_one_roboport_number_of_columns", 5), radius-1)
-		end
-		local only_tile_avail = readSetting("landcreepers_only_place_if_landfill_available", true)
-		local only_robots_avail = readSetting("landcreepers_only_place_if_robots_available", true)
-		for xx = xStart, xStop, 1 do
-			for yy = -radius, radius-1, 1 do
-				local tile = roboport.surface.get_tile(roboport.position.x + xx, roboport.position.y + yy)
-				if only_tile_avail then
-					if only_robots_avail then
-						if hasItem(roboport) and hasRobots(roboport) then
-							placeLandfill(roboport, tile)
-						end
-					else
-						if hasItem(roboport) then
-							placeLandfill(roboport, tile)
-						end
-					end
-				else
-					if only_robots_avail then
-						if hasRobots(roboport) then
-							placeLandfill(roboport, tile)
-						end
-					else
-						placeLandfill(roboport, tile)
-					end
-				end
-			end
-			x = xx
-		end
-		if x == radius-1 then
-			global.landcreepers_roboport_index = global.landcreepers_roboport_index + 1
-			global.landcreepers_roboport_index_x = nil
-		else
-			global.landcreepers_roboport_index_x = x
-		end
-		return
-	else
-		table.remove(global.landcreepers_roboports, global.landcreepers_roboport_index)
-		return
-	end
-end
-
-script.on_nth_tick(600, function()
-	if global.landcreepers_roboports == nil then
-		global.landcreepers_has_run = false
+script.on_nth_tick(60, function()
+	if global.landcreepers == nil then
+		global.landcreepers = {}
 	end
 
-	if not global.landcreepers_has_run then
-		global.landcreepers_roboports = {}
-		for _, surface in pairs(game.surfaces) do
-			for _, roboport in pairs(surface.find_entities_filtered{type="roboport"}) do
-				table.insert(global.landcreepers_roboports, roboport)
+	if global.landcreepers.roboports == nil then
+		global.landcreepers.roboports = {}
+	end
+
+	if #global.landcreepers.roboports == 0 then
+		for _, surfaceItem in pairs(game.surfaces) do
+			for _, roboportItem in pairs(surfaceItem.find_entities_filtered{type="roboport"}) do
+				local rb = {surface=surfaceItem, roboport=roboportItem}
+				table.insert(global.landcreepers.roboports, rb)
 			end
 		end
-		initWaterTiles()
-		global.landcreepers_roboport_index = 1
-		global.landcreepers_has_run = true
+		global.landcreepers.index = 1
 	end
 
-	if (global.landcreepers_roboport_index > #global.landcreepers_roboports) then
-		global.landcreepers_roboport_index = 1
+	if (global.landcreepers.index >= #global.landcreepers.roboports) then
+		global.landcreepers.index = 1
 	end
 
-	landcreep(global.landcreepers_roboports[global.landcreepers_roboport_index])
+	landcreep(global.landcreepers.roboports[global.landcreepers.index])
 end)
 
 local function handleEntityBuiltEvent(event)
 	if event.created_entity and event.created_entity.valid and event.created_entity.type == "roboport" then
-		table.insert(global.landcreepers_roboports, event.created_entity)
+		table.insert(global.landcreepers.roboports, event.created_entity)
 	end
 end
 
--- settings.lua
+++ settings.lua
@@ -8,31 +8,9 @@ data:extend({
   },
   {
     type = "bool-setting",
-    name = "landcreepers_only_place_if_landfill_available",
+    name = "landcreepers_circular_creep",
     setting_type = "runtime-global",
     default_value = true,
     order = "02"
-  },
-  {
-    type = "bool-setting",
-    name = "landcreepers_only_place_if_robots_available",
-    setting_type = "runtime-global",
-    default_value = true,
-    order = "03"
-  },
-  {
-    type = "bool-setting",
-    name = "landcreepers_iterate_less_than_one_roboport",
-    setting_type = "runtime-global",
-    default_value = true,
-    order = "04"
-  },
-  {
-    type = "int-setting",
-    name = "landcreepers_iterate_less_than_one_roboport_number_of_columns",
-    setting_type = "runtime-global",
-    default_value = 5,
-    min_value = 1,
-    order = "04"
   }
 })
